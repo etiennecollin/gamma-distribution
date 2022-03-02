81e4024805e5cbb6a62264f2d6ada02f143db641
etiennecollin <99756528+etiennecollin@users.noreply.github.com>
2022-03-02 15:35:57 -0500

    Update code.

--- src/gamma_2.py
+++ src/gamma_2.py
@@ -1,112 +1,108 @@
-########################
-# f(x) = exp(-x) graph #
-########################
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Create x and y
-x = np.linspace(-2, 20, 100)
-y = np.exp(-x)
+########################
+# f(x) = exp(-x) graph #
+########################
+def gamma_1():
+    # Create x and y
+    x = np.linspace(-2, 20, 100)
+    y = np.exp(-x)
 
-# Create the plot
-fig, ax = plt.subplots()
-plt.plot(x, y, label='f(x) = exp(-x)', linewidth=3, color='palegreen')
+    # Create the plot
+    fig, ax = plt.subplots()
+    plt.plot(x, y, label='f(x) = exp(-x)', linewidth=3, color='palegreen')
 
-# Make the x=0, y=0 thicker
-ax.set_aspect('equal')
-ax.grid(True, which='both')
-ax.axhline(y=0, color='k')
-ax.axvline(x=0, color='k')
+    # Make the x=0, y=0 thicker
+    ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
 
-# Add a title
-plt.title('f(x) = exp(-x)', fontsize=20)
+    # Add a title
+    plt.title('f(x) = exp(-x)', fontsize=20)
 
-# Add X and y Label
-plt.xlabel('x', fontsize=16)
-plt.ylabel('f(x)', fontsize=16)
+    # Add X and y Label
+    plt.xlabel('x', fontsize=16)
+    plt.ylabel('f(x)', fontsize=16)
 
-# Add a grid
-plt.grid(alpha=.4, linestyle='--')
+    # Add a grid
+    plt.grid(alpha=.4, linestyle='--')
 
-# Show the plot
-plt.show()
+    # Show the plot
+    plt.show()
+
 
 ####################
 # f(x) = x^z graph #
 ####################
+def gamma_2():
+    # Create x and y
+    x = np.linspace(0, 2, 100)
+    y1 = x ** 1.3
+    y2 = x ** 2.5
+    y3 = x ** 3.8
 
-import matplotlib.pyplot as plt
-import numpy as np
+    # Create the plot
+    fig, ax = plt.subplots()
+    plt.plot(x, y1, label='f(x) = x^1.3', linewidth=3, color='palegreen')
+    plt.plot(x, y2, label='f(x) = x^2.5', linewidth=3, color='yellowgreen')
+    plt.plot(x, y3, label='f(x) = x^3.8', linewidth=3, color='olivedrab')
 
-# Create x and y
-x = np.linspace(0, 2, 100)
-y1 = x**1.3
-y2 = x**2.5 
-y3 = x**3.8
+    # Make the x=0, y=0 thicker
+    ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
 
-# Create the plot
-fig, ax = plt.subplots()
-plt.plot(x, y1, label='f(x) = x^1.3', linewidth=3, color='palegreen')
-plt.plot(x, y2, label='f(x) = x^2.5', linewidth=3, color='yellowgreen')
-plt.plot(x, y3, label='f(x) = x^3.8', linewidth=3, color='olivedrab')
+    # Add a title
+    plt.title('f(x) = x^z', fontsize=20)
 
-# Make the x=0, y=0 thicker
-ax.set_aspect('equal')
-ax.grid(True, which='both')
-ax.axhline(y=0, color='k')
-ax.axvline(x=0, color='k')
+    # Add X and y Label
+    plt.xlabel('x', fontsize=16)
+    plt.ylabel('f(x)', fontsize=16)
 
-# Add a title
-plt.title('f(x) = x^z', fontsize=20)
+    # Add a grid
+    plt.grid(alpha=.4, linestyle='--')
 
-# Add X and y Label
-plt.xlabel('x', fontsize=16)
-plt.ylabel('f(x)', fontsize=16)
+    # Add a Legend
+    plt.legend(bbox_to_anchor=(1, 1), loc='best', borderaxespad=1, fontsize=12)
 
-# Add a grid
-plt.grid(alpha=.4, linestyle='--')
+    # Show the plot
+    plt.show()
 
-# Add a Legend
-plt.legend(bbox_to_anchor=(1, 1), loc='best', borderaxespad=1, fontsize=12)
-
-# Show the plot
-plt.show()
 
 ###############################
 # f(x) = x^(3.8)*e^(-x) graph #
 ###############################
+def gamma_3():
+    # Create x and y
+    x = np.linspace(0, 20, 100)
+    y = x ** 3.8 * np.exp(-x)
 
-import matplotlib.pyplot as plt
-import numpy as np
+    # Create the plot
+    fig, ax = plt.subplots()
+    plt.plot(x, y, label='f(x) = x^(3.8) * np.exp(-x)', linewidth=3, color='palegreen')
+    ax.fill_between(x, 0, y, color='yellowgreen')
 
-# Create x and y
-x = np.linspace(0, 20, 100)
-y = x**3.8 * np.exp(-x)
+    # Make the x=0, y=0 thicker
+    ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
 
-# Create the plot
-fig, ax = plt.subplots()
-plt.plot(x, y, label='f(x) = x^(3.8) * np.exp(-x)', linewidth=3, color='palegreen')
-ax.fill_between(x, 0, y, color='yellowgreen')
+    # Add a title
+    plt.title('f(x) =  x^(3.8)*e^(-x) ', fontsize=20)
 
-# Make the x=0, y=0 thicker
-ax.set_aspect('equal')
-ax.grid(True, which='both')
-ax.axhline(y=0, color='k')
-ax.axvline(x=0, color='k')
+    # Add X and y Label
+    plt.xlabel('x', fontsize=16)
+    plt.ylabel('f(x)', fontsize=16)
 
-# Add a title
-plt.title('f(x) =  x^(3.8)*e^(-x) ', fontsize=20)
+    # Add a grid
+    plt.grid(alpha=.4, linestyle='--')
 
-# Add X and y Label
-plt.xlabel('x', fontsize=16)
-plt.ylabel('f(x)' ,fontsize=16)
+    # Add a Legend
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=12)
 
-# Add a grid
-plt.grid(alpha=.4, linestyle='--')
-
-# Add a Legend
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=12)
-
-# Show the plot
-plt.show()
+    # Show the plot
+    plt.show()
--- src/vincent_carrier.py
+++ src/vincent_carrier.py
@@ -1,6 +1,48 @@
+import math
 import numpy as np
-from scipy.stats import gamma
 import matplotlib.pyplot as plt
+from scipy.stats import gamma
+import itertools
+
+
+def standard_normal_distribution():
+    # Standard Normal Distribution
+    # vector of values for x: x=(-3.00,-2.99,-2.98, ... ,3.00)
+    x = np.linspace(-3, 3, 601)
+    # vector of values for f(x)
+    fx = (1 / np.sqrt(2 * np.pi)) * np.exp(-(x ** 2) / 2)
+    # graph
+    plt.plot(x, fx)
+    plt.show()
+
+
+def gamma_distribution():
+    # vector of values for x: x=(0.00,0.01,0.02,0.03, ... ,5.00)
+    x = np.linspace(0, 5, 501)
+    # vector of the corresponding values of f(x) if X ~ Gamma(4,3)
+    fx = ((3 ** 4) / math.gamma(4)) * (x ** 3) * np.exp(-3 * x)
+    # value of Delta x: same as gap between values in vector x
+    dx = 0.01
+    # vector of values of [f(x) Delta(x)]
+    fxdx = fx * dx
+    # vector of cumulative values of [f(x) Delta(x)]
+    Fx = np.array(list(itertools.accumulate(fxdx)))
+    # graph of F(x) as a function of x
+    plt.plot(x, Fx)
+    plt.show()
+
+
+def histogram():
+    # x and y are vectors of 1000000 i.i.d. U(0,1) r.v.
+    x = np.random.uniform(0, 1, 1000000)
+    y = np.random.uniform(0, 1, 1000000)
+    # r.v. of interest
+    w = x + y
+    # produces histogram
+    plt.hist(w, bins=100, density=True)
+    # prints histogram
+    plt.show()
+
 def plot_gamma_k():
     """
     k : the number of events for which you are waiting to occur.
@@ -28,6 +70,8 @@ def plot_gamma_k():
     plt.show()
     # plt.savefig('gamma_k.png')
     # plt.clf()
+
+
 def plot_gamma_lambda():
     """
     k : the number of events for which you are waiting to occur.
@@ -36,14 +80,14 @@ def plot_gamma_lambda():
     a = 10  # k = 10
     x = np.linspace(0, 50, 1000)
     lambda_ = 1
-    mean, var, skew, kurt = gamma.stats(a, scale=1/lambda_, moments='mvsk')
-    y1 = gamma.pdf(x, a, scale=1/lambda_)
+    mean, var, skew, kurt = gamma.stats(a, scale=1 / lambda_, moments='mvsk')
+    y1 = gamma.pdf(x, a, scale=1 / lambda_)
     lambda_ = 2
-    mean, var, skew, kurt = gamma.stats(a, scale=1/lambda_, moments='mvsk')
-    y2 = gamma.pdf(x, a, scale=1/lambda_)
+    mean, var, skew, kurt = gamma.stats(a, scale=1 / lambda_, moments='mvsk')
+    y2 = gamma.pdf(x, a, scale=1 / lambda_)
     lambda_ = 3
-    mean, var, skew, kurt = gamma.stats(a, scale=1/lambda_, moments='mvsk')
-    y3 = gamma.pdf(x, a, scale=1/lambda_)
+    mean, var, skew, kurt = gamma.stats(a, scale=1 / lambda_, moments='mvsk')
+    y3 = gamma.pdf(x, a, scale=1 / lambda_)
     plt.title("PDF of Gamma Distribution (k = 10)")
     plt.xlabel("T")
     plt.ylabel("Probability Density")
@@ -57,6 +101,3 @@ def plot_gamma_lambda():
     plt.show()
     # plt.savefig('gamma_lambda.png')
     # plt.clf()
-
-plot_gamma_lambda()
-plot_gamma_k()
